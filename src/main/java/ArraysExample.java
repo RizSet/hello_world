f7481d32030b085a1cdc32c67ddab437ec7ce124
Dmytro Kononenko <rizset290993@gmail.com>
2022-06-23 12:34:25 +0300

    second

---
+++ src/main/java/ArraysExample.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArraysExample {
+    public static void main(String[] args) {
+        int [][] array = new int [4][2];
+
+        array [0][0] = 0;
+        array [0][1] = 1;
+        array [1][0] = 2;
+        array [1][1] = 3;
+        array [2][0] = 4;
+        array [2][1] = 5;
+        array [3][0] = 6;
+        array [3][1] = 7;
+
+        System.out.println(Arrays.toString(array));
+
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javax.security.auth.login.CredentialNotFoundException;
+import java.util.Random;
+
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println();
+
+    }
+}
+
